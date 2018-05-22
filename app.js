f8304874b71f8c37e628485ca7da7d172b30ad07
apramodya <pramodyaabeysinghe@gmail.com>
2018-05-23 02:36:32 +0530

    Init client side

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+app.use(express.static(__dirname + '/client'));
 app.use(bodyParser.json());
 
 Genre = require('./models/genre');
---
+++ client/controllers/books.js
@@ -0,0 +1,5 @@
+var myApp = angular.module('myApp');
+
+myApp.controller('BookController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+    
+}])
