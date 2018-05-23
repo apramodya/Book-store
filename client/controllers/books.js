fed88e5a9a9f955b9775a4893fd689d93f277bd6
apramodya <pramodyaabeysinghe@gmail.com>
2018-05-23 10:04:52 +0530

    Create books view and single book view

--- client/controllers/books.js
+++ client/controllers/books.js
@@ -1,5 +1,17 @@
 var myApp = angular.module('myApp');
 
-myApp.controller('BookController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
-    
-}])
+myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+    console.log('BooksController');
+
+    $scope.getBooks = function(){
+        $http.get('/api/books').then(function(response){
+            $scope.books = response.data;
+        });
+    }
+    $scope.getBook = function(){
+        var id = $routeParams.id;
+        $http.get('/api/book/'+id).then(function(response){
+            $scope.book = response.data;
+        });
+    }
+}]);
